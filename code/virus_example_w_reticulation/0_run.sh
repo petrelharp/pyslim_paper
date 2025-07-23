abb5e36eb71d3d2707722b39d061cb712bdb8ee3
Shyamalika Gopalan <51133211+ShyamieG@users.noreply.github.com>
2025-07-23 16:41:28 -0400

    Update 0_run.sh
    commented to explain expected args

--- code/virus_example_w_reticulation/0_run.sh
+++ code/virus_example_w_reticulation/0_run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# first arg: infection sequence table to use (e.g. inf_seq_w_reticulation.csv)
+# second arg: desired name for snakefile (e.g. snakefile_w_reticulation)
+
 python make_snakefile.py --input $1 --outfile $2
 snakemake -s $2 --rulegraph | dot -Tsvg > DAG.svg
 snakemake -s $2 -c 4
