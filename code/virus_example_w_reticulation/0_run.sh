56758d4a57922504e60558a084348f37bcefd9da
Shyamalika Gopalan <shyamag@clemson.edu>
2025-07-30 17:19:08 -0400

    fixed issues with implementing reticulation - checking for dup ids and slim/ped mismatches

--- code/virus_example_w_reticulation/0_run.sh
+++ code/virus_example_w_reticulation/0_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python make_snakefile.py --input $1 --outfile $2
-snakemake -s $2 --rulegraph | dot -Tsvg > DAG.svg
+#python make_snakefile.py --input $1 --outfile $2
+#snakemake -s $2 --rulegraph | dot -Tsvg > DAG.svg
+rm -f slim_id_update.csv
 snakemake -s $2 -c 4
